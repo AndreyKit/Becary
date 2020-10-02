7e41453c207a3d84028327d8fb02094e41882eb0
AndreyKit <59205429+AndreyKit@users.noreply.github.com>
2020-10-02 17:19:15 +0300

    Final

--- src/Core/Config.php
+++ src/Core/Config.php
@@ -6,7 +6,7 @@ class Config
 {
     const MYSQL_HOST = 'firstproject';
     const MYSQL_USER_NAME = 'root';
-    const MYSQL_PASSWORD = '';
+    const MYSQL_PASSWORD = 'root';
     const MYSQL_DATABASE = 'Becary';
     const PAGE_SIZE = 10;
     const PAGE_SIZE_DISH = 5;
